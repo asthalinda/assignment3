e6bb2553cb0b8e911ed19df4c8f9b4c62a282c07
asthalinda <31248801+asthalinda@users.noreply.github.com>
2017-09-14 16:57:38 +0530

    Add files via upload

--- 3question6.cpp
+++ 3question6.cpp
@@ -1,23 +1,19 @@
 #include<iostream>
 using namespace std;
 int main()
+{ int year;
+  cout<<"enter a year:";
+  cin>>year;
+  if(year%4==0)
 {
- int year;
- cout<< "enter year";
- cin>>year;
- if(year%4==0)
- {
-   if(year%100==0)
-    {
-     if(year%400==0)
-        cout<<"leap year";
-     else
-        cout<<"not leap year";
-    }
-   else
-     cout<<"not a leap year";
- }
- else
-  cout<<"not a leap year";
- return 0;
+  if (year%100==0)
+{
+  if (year%400==0)
+   cout<<year<<"is a leap year";
+  else cout<<year<<"is not a leap year";
+}
+  else cout<<year<<"is a leap year";
+}
+  else cout<<year<<"is not a leap year";
+  return 0;
 }
